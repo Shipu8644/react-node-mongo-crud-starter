f476d908a60a25ffbbe935808f1d73616d90bd93
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-22 20:05:57 +0600

    update running

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,39 @@
 import React from 'react';
-
+import { useRef } from 'react';
 const AddUser = () => {
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+
+    const handleAddUser = (e) => {
+        e.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+
+        const newUser = { name, email };
+        fetch('http://localhost:5000/users', {
+            method: 'post',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert('successfully added the user')
+                    e.target.reset();
+                }
+            })
+    }
     return (
         <div>
-            <h2>This is Add User</h2>
+            <h2>Please Add an User</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" ref={nameRef} />
+                <input type="email" name="" id="" ref={emailRef} />
+                <input type="submit" value="Add" />
+            </form>
         </div>
     );
 };
